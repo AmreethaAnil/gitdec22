3dfe29835f63dc57f35d4fe8c8ca3a8be9594b1f
AmreethaAnil <amreethachauhan@gmail.com>
2023-02-01 07:57:36 +0530

    modified the files

--- src/com/amreetha/automation/gitpractices/Demo.java
+++ src/com/amreetha/automation/gitpractices/Demo.java
@@ -1,5 +1,13 @@
 package com.amreetha.automation.gitpractices;
 
 public class Demo {
+	
+	public void hello() {
+		System.out.println("Joker");
+	}
 
+	public static void main(String[] args) {
+		
+		
+}
 }
--- src/com/amreetha/automation/gitpractices/GitDemoTwo.java
+++ src/com/amreetha/automation/gitpractices/GitDemoTwo.java
@@ -1,5 +1,12 @@
 package com.amreetha.automation.gitpractices;
 
 public class GitDemoTwo {
+	
+	public void hello1() {
+		System.out.println("Killer");
+	}
+	public static void main(String[] args) {
+		
+	}
 
 }
