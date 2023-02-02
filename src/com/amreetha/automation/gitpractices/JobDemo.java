8e98c03a82fed9d0620e6eb056272b51f236db0e
AmreethaAnil <amreethachauhan@gmail.com>
2023-02-02 07:39:03 +0530

    added new jobDemo java file

--- src/com/amreetha/automation/gitpractices/JobDemo.java
+++ src/com/amreetha/automation/gitpractices/JobDemo.java
@@ -3,7 +3,8 @@ package com.amreetha.automation.gitpractices;
 public class JobDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("job");
 
 	}
 
